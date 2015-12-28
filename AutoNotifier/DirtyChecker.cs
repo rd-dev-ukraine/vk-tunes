d1eefd568fdd32ee71218e9975c2dd5f3e48ff4b
Sergey Tokarev <sergey.tokarev@hotmail.com>
2015-12-28 17:05:46 +0200

    Auto INotifyPropertyChanged implementation added

---
+++ AutoNotifier/DirtyChecker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AutoNotifier
+{
+    /// <summary>
+    /// Finds changed properties
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DirtyChecker<T>
+        where T : class
+    {
+        private static readonly IDictionary<string, Func<T, object>> Accessor = BuildAccessor();
+
+        public IDictionary<string, object> GetState(T instance)
+        {
+            if (instance == null)
+                throw new ArgumentNullException(nameof(instance));
+
+            return Accessor.ToDictionary(k => k.Key, k => k.Value(instance));
+        }
+
+        public ISet<string> GetChanges(IDictionary<string, object> previousState, IDictionary<string, object> newState)
+        {
+            if (previousState == null)
+                throw new ArgumentNullException(nameof(previousState));
+            if (newState == null)
+                throw new ArgumentNullException(nameof(newState));
+
+            var result = new HashSet<string>(StringComparer.Ordinal);
+
+            foreach (var kvp in Accessor)
+            {
+                object previousValue = null;
+                object newValue = null;
+
+                previousState.TryGetValue(kvp.Key, out previousValue);
+                newState.TryGetValue(kvp.Key, out newValue);
+
+                if (!EqualityComparer<object>.Default.Equals(previousValue, newValue))
+                    result.Add(kvp.Key);
+            }
+
+            return result;
+        }
+
+        private static IDictionary<string, Func<T, object>> BuildAccessor()
+        {
+            return typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy)
+                .Where(p => p.CanRead)
+                .ToDictionary(
+                    p => p.Name,
+                    p =>
+                    {
+                        var parameter = Expression.Parameter(typeof(T));
+                        return Expression.Lambda<Func<T, object>>(
+                                        Expression.Convert(
+                                            Expression.Property(parameter, p),
+                                            typeof(object)),
+                                        parameter)
+                                   .Compile();
+                    });
+        }
+    }
+}
---
+++ test/AutoNotifier.Test/DirtyCheckerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoNotifier.Test
+{
+    [TestClass]
+    public class DirtyCheckerTest
+    {
+        [TestMethod]
+        public void GetChanges_Success()
+        {
+            // Arrange
+            var checker = new DirtyChecker<Foo>();
+            var foo = new Foo
+            {
+                Value = 10,
+                Name = "Test"
+            };
+
+            var prevState = checker.GetState(foo);
+
+            // Act
+            foo.Value = 20;
+            foo.Name = "Changed";
+
+            var newState = checker.GetState(foo);
+
+            // Assert
+            var changes = checker.GetChanges(prevState, newState);
+
+            Assert.AreEqual(2, changes.Count);
+            Assert.IsTrue(changes.Contains(nameof(foo.Value)));
+            Assert.IsTrue(changes.Contains(nameof(foo.Name)));
+        }
+    }
+
+    public class Foo
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> Address { get; set; }
+    }
+}
